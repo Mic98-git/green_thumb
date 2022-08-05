6f4ead30a5c1ff2d169bea5a8facaa469401d002
ditommaso1801802 <ditommaso.1801802@studenti.uniroma1.it>
2022-08-05 18:35:40 +0200

    modify zoom

--- lib/home.dart
+++ lib/home.dart
@@ -2,7 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-const double ZOOM = 10;
+const double ZOOM = 15;
 
 class HomeView extends StatelessWidget {
   GoogleMapController? mapController;
