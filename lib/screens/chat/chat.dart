7beb75931a79a1bb9a7fffd325825d557cd7b51f
ditommaso1801802 <ditommaso.1801802@studenti.uniroma1.it>
2022-08-22 13:10:01 +0200

    init chat.dart

---
+++ lib/screens/chat/chat.dart
@@ -0,0 +1,281 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:green_thumb/screens/chat/constants.dart';
+import 'package:timeago/timeago.dart';
+import 'package:http/http.dart' as http;
+
+class Message {
+  const Message({
+    required this.messageId,
+    required this.userId,
+    required this.content,
+    required this.createdAt,
+  });
+
+  /// ID of the message
+  final String messageId;
+
+  /// ID of the user who posted the message
+  final String userId;
+
+  /// Text content of the message
+  final String content;
+
+  /// Date and time when the message was created
+  final DateTime createdAt;
+
+  factory Message.fromJson(Map<String, dynamic> json) {
+    return Message(
+        messageId: json['chat']['messageId'],
+        userId: json['chat']['userId'],
+        createdAt: json['chat']['created_at'],
+        content: json['chat']['content']);
+  }
+}
+
+String id = '62fe30f5e7d2a2e6d06ef826';
+
+Future<Message> fetchMessage() async {
+  final response = await http
+      // ignore: prefer_interpolation_to_compose_strings
+      .get(Uri.parse('http://10.0.2.2:3005/chat/' + id));
+
+  if (response.statusCode == 200) {
+    print(response.body);
+    return Message.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception('Failed to load message');
+  }
+}
+
+/// Page to chat with someone.
+///
+/// Displays chat bubbles as a ListView and TextField to enter new chat.
+class ChatPage extends StatefulWidget {
+  const ChatPage({Key? key}) : super(key: key);
+
+  static Route<void> route() {
+    return MaterialPageRoute(
+      builder: (context) => const ChatPage(),
+    );
+  }
+
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  late final Stream<List<Message>> _messagesStream;
+  final Map<String, Profile> _profileCache = {};
+
+  @override
+  void initState() {
+    final myUserId = supabase.auth.currentUser!.id;
+    _messagesStream = supabase
+        .from('messages')
+        .stream(['id'])
+        .order('created_at')
+        .execute()
+        .map((maps) => maps
+            .map((map) => Message.fromMap(map: map, myUserId: myUserId))
+            .toList());
+    super.initState();
+  }
+
+  Future<void> _loadProfileCache(String profileId) async {
+    if (_profileCache[profileId] != null) {
+      return;
+    }
+    final res = await supabase
+        .from('profiles')
+        .select()
+        .match({'id': profileId})
+        .single()
+        .execute();
+    final data = res.data;
+    if (data != null) {
+      final profile = Profile.fromMap(data);
+      setState(() {
+        _profileCache[profileId] = profile;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Chat')),
+      body: StreamBuilder<List<Message>>(
+        stream: _messagesStream,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            final messages = snapshot.data!;
+            return Column(
+              children: [
+                Expanded(
+                  child: messages.isEmpty
+                      ? const Center(
+                          child: Text('Start your conversation now :)'),
+                        )
+                      : ListView.builder(
+                          reverse: true,
+                          itemCount: messages.length,
+                          itemBuilder: (context, index) {
+                            final message = messages[index];
+
+                            /// I know it's not good to include code that is not related
+                            /// to rendering the widget inside build method, but for
+                            /// creating an app quick and dirty, it's fine 😂
+                            _loadProfileCache(message.profileId);
+
+                            return _ChatBubble(
+                              message: message,
+                              profile: _profileCache[message.profileId],
+                            );
+                          },
+                        ),
+                ),
+                const _MessageBar(),
+              ],
+            );
+          } else {
+            return preloader;
+          }
+        },
+      ),
+    );
+  }
+}
+
+/// Set of widget that contains TextField and Button to submit message
+class _MessageBar extends StatefulWidget {
+  const _MessageBar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<_MessageBar> createState() => _MessageBarState();
+}
+
+class _MessageBarState extends State<_MessageBar> {
+  late final TextEditingController _textController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.grey[200],
+      child: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [
+              Expanded(
+                child: TextFormField(
+                  keyboardType: TextInputType.text,
+                  maxLines: null,
+                  autofocus: true,
+                  controller: _textController,
+                  decoration: const InputDecoration(
+                    hintText: 'Type a message',
+                    border: InputBorder.none,
+                    focusedBorder: InputBorder.none,
+                    contentPadding: EdgeInsets.all(8),
+                  ),
+                ),
+              ),
+              TextButton(
+                onPressed: () => _submitMessage(),
+                child: const Text('Send'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    _textController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _textController.dispose();
+    super.dispose();
+  }
+
+  void _submitMessage() async {
+    final text = _textController.text;
+    final myUserId = supabase.auth.currentUser!.id;
+    if (text.isEmpty) {
+      return;
+    }
+    _textController.clear();
+
+    final res = await supabase.from('messages').insert({
+      'profile_id': myUserId,
+      'content': text,
+    }).execute();
+    final error = res.error;
+    if (error != null) {
+      context.showErrorSnackBar(message: error.message);
+    }
+  }
+}
+
+class _ChatBubble extends StatelessWidget {
+  const _ChatBubble({
+    Key? key,
+    required this.message,
+    required this.profile,
+  }) : super(key: key);
+
+  final Message message;
+  final Profile? profile;
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> chatContents = [
+      if (!message.isMine)
+        CircleAvatar(
+          child: profile == null
+              ? preloader
+              : Text(profile!.username.substring(0, 2)),
+        ),
+      const SizedBox(width: 12),
+      Flexible(
+        child: Container(
+          padding: const EdgeInsets.symmetric(
+            vertical: 8,
+            horizontal: 12,
+          ),
+          decoration: BoxDecoration(
+            color: message.isMine
+                ? Theme.of(context).primaryColor
+                : Colors.grey[300],
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: Text(message.content),
+        ),
+      ),
+      const SizedBox(width: 12),
+      Text(format(message.createdAt, locale: 'en_short')),
+      const SizedBox(width: 60),
+    ];
+    if (message.isMine) {
+      chatContents = chatContents.reversed.toList();
+    }
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 18),
+      child: Row(
+        mainAxisAlignment:
+            message.isMine ? MainAxisAlignment.end : MainAxisAlignment.start,
+        children: chatContents,
+      ),
+    );
+  }
+}
