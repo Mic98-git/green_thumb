b99090e24dd0bed8186f49894f9e117b6c487d1d
ditommaso1801802 <ditommaso.1801802@studenti.uniroma1.it>
2022-08-23 16:49:58 +0200

    v2

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,67 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:green_thumb/screens/chat/chat.dart';
 import 'package:green_thumb/screens/login_page.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'custom_material_color.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
-  runApp(const MyApp());
+  runApp(MyApp());
   FlutterNativeSplash.remove();
 }
 
+class Profile {
+  const Profile({required this.fullname});
+
+  /// Username of the profile
+  final String fullname;
+
+  factory Profile.fromJson(Map<String, dynamic> json) {
+    return Profile(fullname: json['user']['fullname']);
+  }
+}
+
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
+
+  String idDestinatario = '62fe30611f401e001333dd93';
+
+  Future<Profile> getUsername() async {
+    final response = await http
+        // ignore: prefer_interpolation_to_compose_strings
+        .get(Uri.parse('http://10.0.2.2:3000/users/' + idDestinatario));
+
+    if (response.statusCode == 200) {
+      print(response.body);
+      return Profile.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to load message');
+    }
+  }
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    Future<Profile> destinatario = getUsername();
+    AsyncSnapshot snapshot = new AsyncSnapshot.waiting();
+    if (snapshot.hasData) {
+      String fullname = snapshot.data.fullname;
+      String id = snapshot.data.id;
+    }
     return MaterialApp(
       title: 'GreenThumb ecommerce',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: createMaterialColor(Color.fromRGBO(51, 153, 66, 1)),
       ),
-      home: const LoginScreen(),
+      home: ChatPage(
+        id: "ciao",
+        username: "ciao",
+      ),
     );
   }
 }
--- lib/screens/chat/chat.dart
+++ lib/screens/chat/chat.dart
@@ -2,17 +2,14 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:green_thumb/screens/chat/constants.dart';
-import 'package:timeago/timeago.dart';
 import 'package:http/http.dart' as http;
 
 class Message {
-  const Message({
-    required this.messageId,
-    required this.userId,
-    required this.content,
-    required this.createdAt,
-  });
+  const Message(
+      {required this.messageId,
+      required this.userId,
+      required this.content,
+      required this.createdAt});
 
   /// ID of the message
   final String messageId;
@@ -24,258 +21,207 @@ class Message {
   final String content;
 
   /// Date and time when the message was created
-  final DateTime createdAt;
+  final String createdAt;
 
   factory Message.fromJson(Map<String, dynamic> json) {
     return Message(
-        messageId: json['chat']['messageId'],
-        userId: json['chat']['userId'],
-        createdAt: json['chat']['created_at'],
-        content: json['chat']['content']);
+        messageId: json['mess'][0]['_id'],
+        userId: json['mess'][0]['userId'],
+        createdAt: json['mess'][0]['created_at'],
+        content: json['mess'][0]['content']);
   }
 }
 
-String id = '62fe30f5e7d2a2e6d06ef826';
-
-Future<Message> fetchMessage() async {
-  final response = await http
-      // ignore: prefer_interpolation_to_compose_strings
-      .get(Uri.parse('http://10.0.2.2:3005/chat/' + id));
-
-  if (response.statusCode == 200) {
-    print(response.body);
-    return Message.fromJson(jsonDecode(response.body));
-  } else {
-    throw Exception('Failed to load message');
-  }
-}
-
-/// Page to chat with someone.
-///
-/// Displays chat bubbles as a ListView and TextField to enter new chat.
 class ChatPage extends StatefulWidget {
-  const ChatPage({Key? key}) : super(key: key);
+  String id;
+  String username;
 
-  static Route<void> route() {
-    return MaterialPageRoute(
-      builder: (context) => const ChatPage(),
-    );
-  }
+  ChatPage({
+    required this.id,
+    required this.username,
+  });
 
   @override
-  State<ChatPage> createState() => _ChatPageState();
+  _ChatPageState createState() => _ChatPageState();
 }
 
 class _ChatPageState extends State<ChatPage> {
-  late final Stream<List<Message>> _messagesStream;
-  final Map<String, Profile> _profileCache = {};
-
-  @override
-  void initState() {
-    final myUserId = supabase.auth.currentUser!.id;
-    _messagesStream = supabase
-        .from('messages')
-        .stream(['id'])
-        .order('created_at')
-        .execute()
-        .map((maps) => maps
-            .map((map) => Message.fromMap(map: map, myUserId: myUserId))
-            .toList());
-    super.initState();
-  }
-
-  Future<void> _loadProfileCache(String profileId) async {
-    if (_profileCache[profileId] != null) {
-      return;
-    }
-    final res = await supabase
-        .from('profiles')
-        .select()
-        .match({'id': profileId})
-        .single()
-        .execute();
-    final data = res.data;
-    if (data != null) {
-      final profile = Profile.fromMap(data);
-      setState(() {
-        _profileCache[profileId] = profile;
-      });
-    }
-  }
+  TextEditingController _ctrlMess = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Chat')),
-      body: StreamBuilder<List<Message>>(
-        stream: _messagesStream,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            final messages = snapshot.data!;
+      appBar: AppBar(
+        title: Text(widget.username),
+      ),
+      body: StreamBuilder(
+        initialData: null,
+        stream: getStream(),
+        builder: (context, AsyncSnapshot snap) {
+          if (snap.hasData) {
             return Column(
-              children: [
-                Expanded(
-                  child: messages.isEmpty
-                      ? const Center(
-                          child: Text('Start your conversation now :)'),
-                        )
-                      : ListView.builder(
-                          reverse: true,
-                          itemCount: messages.length,
+              children: <Widget>[
+                StreamBuilder(
+                  builder: (context, snap) {
+                    List<dynamic>? lst = snap.data as List?;
+                    if (lst != null) {
+                      return Expanded(
+                        child: ListView.builder(
+                          itemCount: lst.length,
                           itemBuilder: (context, index) {
-                            final message = messages[index];
-
-                            /// I know it's not good to include code that is not related
-                            /// to rendering the widget inside build method, but for
-                            /// creating an app quick and dirty, it's fine 😂
-                            _loadProfileCache(message.profileId);
-
-                            return _ChatBubble(
-                              message: message,
-                              profile: _profileCache[message.profileId],
+                            var username = lst[index]['user'];
+                            var mess = lst[index]['content'].toString();
+                            return Container(
+                              margin: username == widget.username
+                                  ? EdgeInsets.only(
+                                      right: 2, bottom: 5, top: 5, left: 100)
+                                  : EdgeInsets.only(
+                                      right: 100, bottom: 5, top: 5, left: 2),
+                              padding: EdgeInsets.all(13),
+                              child: Text(
+                                mess,
+                                textAlign: username == widget.username
+                                    ? TextAlign.right
+                                    : TextAlign.left,
+                                style: TextStyle(
+                                    color: username == widget.username
+                                        ? Colors.white
+                                        : Colors.black),
+                              ),
+                              decoration: BoxDecoration(
+                                color: username == widget.username
+                                    ? Colors.blue[400]
+                                    : Colors.blueGrey[100],
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(10)),
+                              ),
                             );
                           },
                         ),
+                      );
+                    } else {
+                      return Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    }
+                  },
                 ),
-                const _MessageBar(),
+                Divider(
+                  color: Colors.blueAccent,
+                ),
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                        child: TextField(
+                      controller: _ctrlMess,
+                      decoration:
+                          new InputDecoration.collapsed(hintText: "Write Here"),
+                    )),
+                    Container(
+                      child: IconButton(
+                          icon: Icon(
+                            Icons.send,
+                            color: Colors.blue,
+                          ),
+                          onPressed: () async {
+                            String content = _ctrlMess.text.trim();
+                            if (content.isNotEmpty) {
+                              var res = await sendMessage(content);
+                              print(res);
+                              _ctrlMess.text = "";
+                            } else {
+                              print("empty");
+                            }
+                          }),
+                    )
+                  ],
+                )
               ],
             );
           } else {
-            return preloader;
+            return Center(
+              child: CircularProgressIndicator(),
+            );
           }
         },
       ),
     );
   }
-}
 
-/// Set of widget that contains TextField and Button to submit message
-class _MessageBar extends StatefulWidget {
-  const _MessageBar({
-    Key? key,
-  }) : super(key: key);
+  String idDestinatario = '62fe30f5e7d2a2e6d06ef826';
+  String idMittente = '62fe30611f401e001333dd93';
 
-  @override
-  State<_MessageBar> createState() => _MessageBarState();
-}
+  Future<Message> sendMessage(String content) async {
+    final response = await http.post(Uri.parse('http://10.0.2.2:3005/chat'),
+        body: {'userId': idMittente, 'content': content});
 
-class _MessageBarState extends State<_MessageBar> {
-  late final TextEditingController _textController;
-
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      color: Colors.grey[200],
-      child: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Row(
-            children: [
-              Expanded(
-                child: TextFormField(
-                  keyboardType: TextInputType.text,
-                  maxLines: null,
-                  autofocus: true,
-                  controller: _textController,
-                  decoration: const InputDecoration(
-                    hintText: 'Type a message',
-                    border: InputBorder.none,
-                    focusedBorder: InputBorder.none,
-                    contentPadding: EdgeInsets.all(8),
-                  ),
-                ),
-              ),
-              TextButton(
-                onPressed: () => _submitMessage(),
-                child: const Text('Send'),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  @override
-  void initState() {
-    _textController = TextEditingController();
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _textController.dispose();
-    super.dispose();
-  }
-
-  void _submitMessage() async {
-    final text = _textController.text;
-    final myUserId = supabase.auth.currentUser!.id;
-    if (text.isEmpty) {
-      return;
-    }
-    _textController.clear();
-
-    final res = await supabase.from('messages').insert({
-      'profile_id': myUserId,
-      'content': text,
-    }).execute();
-    final error = res.error;
-    if (error != null) {
-      context.showErrorSnackBar(message: error.message);
+    if (response.statusCode == 200) {
+      print(response.body);
+      return Message.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to load message');
     }
   }
-}
 
-class _ChatBubble extends StatelessWidget {
-  const _ChatBubble({
-    Key? key,
-    required this.message,
-    required this.profile,
-  }) : super(key: key);
+  Future<Message> getMessage() async {
+    final response = await http
+        // ignore: prefer_interpolation_to_compose_strings
+        .get(Uri.parse('http://10.0.2.2:3005/chat/' + idMittente));
 
-  final Message message;
-  final Profile? profile;
-
-  @override
-  Widget build(BuildContext context) {
-    List<Widget> chatContents = [
-      if (!message.isMine)
-        CircleAvatar(
-          child: profile == null
-              ? preloader
-              : Text(profile!.username.substring(0, 2)),
-        ),
-      const SizedBox(width: 12),
-      Flexible(
-        child: Container(
-          padding: const EdgeInsets.symmetric(
-            vertical: 8,
-            horizontal: 12,
-          ),
-          decoration: BoxDecoration(
-            color: message.isMine
-                ? Theme.of(context).primaryColor
-                : Colors.grey[300],
-            borderRadius: BorderRadius.circular(8),
-          ),
-          child: Text(message.content),
-        ),
-      ),
-      const SizedBox(width: 12),
-      Text(format(message.createdAt, locale: 'en_short')),
-      const SizedBox(width: 60),
-    ];
-    if (message.isMine) {
-      chatContents = chatContents.reversed.toList();
+    if (response.statusCode == 200) {
+      print(response.body);
+      return Message.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to load message');
     }
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 18),
-      child: Row(
-        mainAxisAlignment:
-            message.isMine ? MainAxisAlignment.end : MainAxisAlignment.start,
-        children: chatContents,
-      ),
-    );
   }
+
+  // Future<List<dynamic>> _getData(int value) async {
+  //   var res =
+  //       await http.post('http://192.168.1.107/chat/api/get_mess.php', body: {
+  //     'username': widget.user.username,
+  //     'password': widget.user.password,
+  //     'user_to': widget.userTo.username
+  //   });
+  //   var jsonx = json.decode(res.body);
+  //   return jsonx;
+  // }
+
+  Stream<dynamic> getStream() async* {
+    yield* Stream.periodic(Duration(seconds: 1), (_) {
+      return getMessage();
+    }).asyncMap((event) async => await event);
+  }
+
+  // Stream<Future<List<dynamic>>> _stream() {
+  //   Duration interval = Duration(seconds: 1);
+  //   // Stream<Future<List<dynamic>>> stream =
+  //   //     Stream<Future<List<dynamic>>>.periodic(interval, getMessage());
+  //    Stream<Future<List<dynamic>>> stream = getMessage() as Stream<Future<List>>;
+  //   return stream;
+  // }
+
+  // Future<Profile> getUsername() async {
+  //   final response = await http
+  //       // ignore: prefer_interpolation_to_compose_strings
+  //       .get(Uri.parse('http://10.0.2.2:3000/users/' + idMittente));
+
+  //   if (response.statusCode == 200) {
+  //     print(response.body);
+  //     return Profile.fromJson(jsonDecode(response.body));
+  //   } else {
+  //     throw Exception('Failed to load message');
+  //   }
+  // }
+
+  // _sendMess(String content) async {
+  //   var res =
+  //       await http.post('http://192.168.1.107/chat/api/send_mess.php', body: {
+  //     'username': widget.user.username,
+  //     'password': widget.user.password,
+  //     'user_to': widget.userTo.username,
+  //     'content': content
+  //   });
+  //   return res.body;
+  // }
 }
