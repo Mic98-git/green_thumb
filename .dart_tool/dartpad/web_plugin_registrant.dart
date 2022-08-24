5e66d57136d1bb7d6b5de77666296cded9c5dbad
ditommaso1801802 <ditommaso.1801802@studenti.uniroma1.it>
2022-08-24 16:43:23 +0200

    fix send message

--- .dart_tool/dartpad/web_plugin_registrant.dart
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -3,15 +3,20 @@
 // Generated file. Do not edit.
 //
 
+// @dart = 2.13
 // ignore_for_file: type=lint
 
+import 'package:file_picker/_internal/file_picker_web.dart';
 import 'package:flutter_native_splash/flutter_native_splash_web.dart';
 import 'package:image_picker_for_web/image_picker_for_web.dart';
+import 'package:url_launcher_web/url_launcher_web.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 void registerPlugins([final Registrar? pluginRegistrar]) {
   final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
+  FilePickerWeb.registerWith(registrar);
   FlutterNativeSplashWeb.registerWith(registrar);
   ImagePickerPlugin.registerWith(registrar);
+  UrlLauncherPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
 }
--- lib/screens/chat/chat.dart
+++ lib/screens/chat/chat.dart
@@ -2,6 +2,8 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
+import 'package:flutter_chat_ui/flutter_chat_ui.dart';
 import 'package:http/http.dart' as http;
 
 class Message {
@@ -32,196 +34,114 @@ class Message {
   }
 }
 
-class ChatPage extends StatefulWidget {
-  String id;
-  String username;
+String idDestinatario = '62fe30f5e7d2a2e6d06ef826';
+String idMittente = '62fe30611f401e001333dd93';
 
-  ChatPage({
-    required this.id,
-    required this.username,
-  });
+class Profile {
+  const Profile({required this.fullname, required this.userId});
 
-  @override
-  _ChatPageState createState() => _ChatPageState();
+  /// Username of the profile
+  final String fullname;
+
+  final String userId;
+
+  String get getName {
+    return fullname;
+  }
+
+  factory Profile.fromJson(Map<String, dynamic> json) {
+    return Profile(
+        fullname: json['user']['fullname'], userId: json['user']['_id']);
+  }
 }
 
-class _ChatPageState extends State<ChatPage> {
-  TextEditingController _ctrlMess = TextEditingController();
+Future<Profile> getUsername() async {
+  final response = await http
+      // ignore: prefer_interpolation_to_compose_strings
+      .get(Uri.parse('http://10.0.2.2:3000/users/' + idMittente));
+
+  if (response.statusCode == 200) {
+    print(response.body);
+    return await Profile.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception('Failed to load message');
+  }
+}
+
+Future<http.Response> sendMessage(String content, String idDestinatario) {
+  return http.post(
+    Uri.parse('http://10.0.2.2:3005/chat/'),
+    body: (<String, String>{'userId': idDestinatario, 'content': content}),
+  );
+}
+
+// Future<Message> getMessage() async {
+//   final response = await http
+//       // ignore: prefer_interpolation_to_compose_strings
+//       .get(Uri.parse('http://10.0.2.2:3005/chat/' + idMittente));
+
+//   if (response.statusCode == 200) {
+//     print(response.body);
+//     return Message.fromJson(jsonDecode(response.body));
+//   } else {
+//     throw Exception('Failed to load message');
+//   }
+// }
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.username),
-      ),
-      body: StreamBuilder(
-        initialData: null,
-        stream: getStream(),
-        builder: (context, AsyncSnapshot snap) {
-          if (snap.hasData) {
-            return Column(
-              children: <Widget>[
-                StreamBuilder(
-                  builder: (context, snap) {
-                    List<dynamic>? lst = snap.data as List?;
-                    if (lst != null) {
-                      return Expanded(
-                        child: ListView.builder(
-                          itemCount: lst.length,
-                          itemBuilder: (context, index) {
-                            var username = lst[index]['user'];
-                            var mess = lst[index]['content'].toString();
-                            return Container(
-                              margin: username == widget.username
-                                  ? EdgeInsets.only(
-                                      right: 2, bottom: 5, top: 5, left: 100)
-                                  : EdgeInsets.only(
-                                      right: 100, bottom: 5, top: 5, left: 2),
-                              padding: EdgeInsets.all(13),
-                              child: Text(
-                                mess,
-                                textAlign: username == widget.username
-                                    ? TextAlign.right
-                                    : TextAlign.left,
-                                style: TextStyle(
-                                    color: username == widget.username
-                                        ? Colors.white
-                                        : Colors.black),
-                              ),
-                              decoration: BoxDecoration(
-                                color: username == widget.username
-                                    ? Colors.blue[400]
-                                    : Colors.blueGrey[100],
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(10)),
-                              ),
-                            );
-                          },
-                        ),
-                      );
-                    } else {
-                      return Center(
-                        child: CircularProgressIndicator(),
-                      );
-                    }
-                  },
-                ),
-                Divider(
-                  color: Colors.blueAccent,
-                ),
-                Row(
-                  children: <Widget>[
-                    Expanded(
-                        child: TextField(
-                      controller: _ctrlMess,
-                      decoration:
-                          new InputDecoration.collapsed(hintText: "Write Here"),
-                    )),
-                    Container(
-                      child: IconButton(
-                          icon: Icon(
-                            Icons.send,
-                            color: Colors.blue,
-                          ),
-                          onPressed: () async {
-                            String content = _ctrlMess.text.trim();
-                            if (content.isNotEmpty) {
-                              var res = await sendMessage(content);
-                              print(res);
-                              _ctrlMess.text = "";
-                            } else {
-                              print("empty");
-                            }
-                          }),
-                    )
-                  ],
-                )
-              ],
-            );
-          } else {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-        },
-      ),
-    );
+  Widget build(BuildContext context) => const MaterialApp(
+        home: MyHomePage(),
+      );
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<types.Message> _messages = [];
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        body: Chat(
+          messages: _messages,
+          onSendPressed: _handleSendPressed,
+          user: types.User(id: "user.userId", firstName: "user.fullname"),
+        ),
+      );
+
+  void _addMessage(types.Message message) {
+    setState(() {
+      _messages.insert(0, message);
+    });
   }
 
   String idDestinatario = '62fe30f5e7d2a2e6d06ef826';
-  String idMittente = '62fe30611f401e001333dd93';
 
-  Future<Message> sendMessage(String content) async {
-    final response = await http.post(Uri.parse('http://10.0.2.2:3005/chat'),
-        body: {'userId': idMittente, 'content': content});
+  void _handleSendPressed(types.PartialText message) {
+    final textMessage = types.TextMessage(
+      author: types.User(id: "user.userId", firstName: "user.fullname"),
+      createdAt: DateTime.now().millisecondsSinceEpoch,
+      id: idDestinatario,
+      text: message.text,
+    );
 
-    if (response.statusCode == 200) {
-      print(response.body);
-      return Message.fromJson(jsonDecode(response.body));
-    } else {
-      throw Exception('Failed to load message');
-    }
+    print(message.text);
+
+    String content = message.text;
+
+    sendMessage(content, idDestinatario);
+
+    _addMessage(textMessage);
   }
-
-  Future<Message> getMessage() async {
-    final response = await http
-        // ignore: prefer_interpolation_to_compose_strings
-        .get(Uri.parse('http://10.0.2.2:3005/chat/' + idMittente));
-
-    if (response.statusCode == 200) {
-      print(response.body);
-      return Message.fromJson(jsonDecode(response.body));
-    } else {
-      throw Exception('Failed to load message');
-    }
-  }
-
-  // Future<List<dynamic>> _getData(int value) async {
-  //   var res =
-  //       await http.post('http://192.168.1.107/chat/api/get_mess.php', body: {
-  //     'username': widget.user.username,
-  //     'password': widget.user.password,
-  //     'user_to': widget.userTo.username
-  //   });
-  //   var jsonx = json.decode(res.body);
-  //   return jsonx;
-  // }
-
-  Stream<dynamic> getStream() async* {
-    yield* Stream.periodic(Duration(seconds: 1), (_) {
-      return getMessage();
-    }).asyncMap((event) async => await event);
-  }
-
-  // Stream<Future<List<dynamic>>> _stream() {
-  //   Duration interval = Duration(seconds: 1);
-  //   // Stream<Future<List<dynamic>>> stream =
-  //   //     Stream<Future<List<dynamic>>>.periodic(interval, getMessage());
-  //    Stream<Future<List<dynamic>>> stream = getMessage() as Stream<Future<List>>;
-  //   return stream;
-  // }
-
-  // Future<Profile> getUsername() async {
-  //   final response = await http
-  //       // ignore: prefer_interpolation_to_compose_strings
-  //       .get(Uri.parse('http://10.0.2.2:3000/users/' + idMittente));
-
-  //   if (response.statusCode == 200) {
-  //     print(response.body);
-  //     return Profile.fromJson(jsonDecode(response.body));
-  //   } else {
-  //     throw Exception('Failed to load message');
-  //   }
-  // }
-
-  // _sendMess(String content) async {
-  //   var res =
-  //       await http.post('http://192.168.1.107/chat/api/send_mess.php', body: {
-  //     'username': widget.user.username,
-  //     'password': widget.user.password,
-  //     'user_to': widget.userTo.username,
-  //     'content': content
-  //   });
-  //   return res.body;
-  // }
 }
