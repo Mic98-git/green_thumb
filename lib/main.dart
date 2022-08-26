7f39bb6adedf88a249da42ee046fbd13b846d63e
ditommaso1801802 <ditommaso.1801802@studenti.uniroma1.it>
2022-08-26 15:54:50 +0200

    fix sendMessage

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:green_thumb/screens/chat/chat.dart';
 import 'package:green_thumb/screens/login_page.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import './screens/my_account.dart';
---
+++ lib/models/user.dart
@@ -0,0 +1,17 @@
+class User {
+  const User({required this.fullname, required this.userId});
+
+  /// Username of the profile
+  final String fullname;
+
+  final String userId;
+
+  String get getName {
+    return fullname;
+  }
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+        fullname: json['user']['fullname'], userId: json['user']['_id']);
+  }
+}
